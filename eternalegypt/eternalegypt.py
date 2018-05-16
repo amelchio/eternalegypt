0de062a46dd6c094377ceeb3b3ca59a400582da6
Anders Melchiorsen <amelchio@nogoto.net>
2018-05-17 01:32:59 +0200

    Use objects for returned information

--- eternalegypt/eternalegypt.py
+++ eternalegypt/eternalegypt.py
@@ -5,9 +5,21 @@ import json
 import datetime
 import aiohttp
 import async_timeout
+import attr
 
 _LOGGER = logging.getLogger(__name__)
 
+@attr.s
+class SMS:
+    timestamp = attr.ib()
+    unread = attr.ib()
+    sender = attr.ib()
+    message = attr.ib()
+
+@attr.s
+class Information:
+    sms = attr.ib(factory=list)
+    usage = attr.ib(default=None)
 
 class LB2120:
     """Class for Netgear LB2120 interface."""
@@ -66,25 +78,20 @@ class LB2120:
     async def information(self):
         """Return the SMS inbox."""
         session = await self.login()
-        result = { 'sms': [] }
+        result = Information()
 
         async with async_timeout.timeout(10):
             url = self.url('model.json')
             async with session.get(url) as response:
                 data = json.loads(await response.text())
 
-                result['usage'] = data['wwan']['dataUsage']['generic']['dataTransferred']
+                result.usage = data['wwan']['dataUsage']['generic']['dataTransferred']
 
                 for msg in [m for m in data['sms']['msgs'] if 'text' in m]:
                     # {'id': '6', 'rxTime': '11/03/18 08:18:11 PM', 'text': 'tak tik', 'sender': '555-987-654', 'read': False}
                     dt = datetime.datetime.strptime(msg['rxTime'], '%d/%m/%y %I:%M:%S %p')
-                    element = {
-                        'timestamp': dt,
-                        'unread': not msg['read'],
-                        'sender': msg['sender'],
-                        'message': msg['text'],
-                    }
-                    result['sms'].append(element)
+                    element = SMS(dt, not msg['read'], msg['sender'], msg['text'])
+                    result.sms.append(element)
 
         await session.close()
 
--- examples/inbox.py
+++ examples/inbox.py
@@ -5,6 +5,7 @@
 import sys
 import asyncio
 import logging
+import pprint
 
 import eternalegypt
 
@@ -12,14 +13,14 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 async def get_information():
-    """Example of sending a message."""
+    """Example of printing the inbox."""
     modem = eternalegypt.LB2120(
         hostname=sys.argv[1],
         password=sys.argv[2])
 
     result = await modem.information()
-    import pprint
-    pprint.pprint(result)
+    for sms in result.sms:
+        pprint.pprint(sms)
 
 if len(sys.argv) != 3:
     print("{}: <lb2120 ip> <lb2120 password>".format(sys.argv[0]))
