e925ba85ae9c36fd2de40e2b4e1fbadc9f5cf35c
Anders Melchiorsen <amelchio@nogoto.net>
2018-06-23 16:00:27 +0200

    Version 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     name='eternalegypt',
     packages=['eternalegypt'],
-    version='0.0.1',
+    version='0.0.2',
     install_requires=['aiohttp>=3.0.1','attrs'],
     description='Netgear LTE modem API',
     author='Anders Melchiorsen',
