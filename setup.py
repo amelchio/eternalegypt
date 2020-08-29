366be2ddd527e3b59a41a5b174a3ff627a5dde16
Anders Melchiorsen <amelchio@nogoto.net>
2020-08-29 19:30:18 +0200

    Version 0.0.12

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@
 from setuptools import setup
 
 setup(
-    name='eternalegypt',
-    packages=['eternalegypt'],
-    version='0.0.11',
-    install_requires=['aiohttp>=3.0.1','attrs'],
-    description='Netgear LTE modem API',
-    author='Anders Melchiorsen',
-    author_email='amelchio@nogoto.net',
-    url='https://github.com/amelchio/eternalegypt',
-    license='MIT',
-    keywords=['netgear,lte,lb1120,lb2120'],
+    name="eternalegypt",
+    packages=["eternalegypt"],
+    version="0.0.12",
+    install_requires=["aiohttp>=3.0.1","attrs"],
+    description="Netgear LTE modem API",
+    author="Anders Melchiorsen",
+    author_email="amelchio@nogoto.net",
+    url="https://github.com/amelchio/eternalegypt",
+    license="MIT",
+    keywords=["netgear,lte,lb1120,lb2120"],
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Intended Audience :: Developers",
