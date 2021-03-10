6cd6f543a4e471fd7abb32432569beac704ae2b4
Alyssa Smith <63546997+alyssadev@users.noreply.github.com>
2021-03-10 20:00:39 +0800

    Add SMS forwarder example

---
+++ examples/sms_forward.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+"""Example file for Eternal Egypt library."""
+
+import sys
+import asyncio
+import aiohttp
+import logging
+
+import eternalegypt
+
+
+async def wait_for_messages():
+    jar = aiohttp.CookieJar(unsafe=True)
+    websession = aiohttp.ClientSession(cookie_jar=jar)
+
+    modem = eternalegypt.Modem(hostname=sys.argv[1], websession=websession)
+    await modem.login(password=sys.argv[2])
+    def forward_sms(sms):
+        asyncio.get_event_loop().create_task(modem.sms(phone=sys.argv[3], message=f"{sms.sender}: {sms.message}"))
+    await modem.add_sms_listener(forward_sms)
+
+    try:
+        while True:
+            await modem.information() # sends new sms objects to listener
+            await asyncio.sleep(5)
+    finally:
+        await modem.logout()
+        await websession.close()
+
+if len(sys.argv) != 4:
+    print("{}: <netgear ip> <netgear password> <phone>".format(sys.argv[0]))
+else:
+    asyncio.get_event_loop().run_until_complete(wait_for_messages())
